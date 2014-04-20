1650d4abb1f79d43d24c7c98368bfcb60ad63ca5
maleber-net <gun_ip@yahoo.com>
2014-04-20 16:04:05 +0700

    Update maleber.js

--- javascripts/maleber.js
+++ javascripts/maleber.js
@@ -2,7 +2,7 @@ function clearDesc(obj) {
   
   var str = obj.innerHTML;
   str = str.replace(/<(?:.|\n)*?>/gm, '');
-  str = str.substring(0, 400);
+  str = str.substring(0, 300);
   var lastIndex = str.lastIndexOf(" ")
   str = str.substring(0, lastIndex);
   obj.innerHTML = str;
