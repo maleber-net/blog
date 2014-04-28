9003d8ec5697edc012add571d65d576a1f5355c0
maleber-net <gun_ip@yahoo.com>
2014-04-28 13:23:58 +0700

    Update maleber.js

--- javascripts/maleber.js
+++ javascripts/maleber.js
@@ -7,3 +7,13 @@ function clearDesc(obj) {
   str = str.substring(0, lastIndex);
   obj.innerHTML = str;
 }
+
+function clearDescById(id) {
+  var obj = document.getElementById(id);
+  var str = obj.innerHTML;
+  str = str.replace(/<(?:.|\n)*?>/gm, '');
+  str = str.substring(0, 300);
+  var lastIndex = str.lastIndexOf(" ")
+  str = str.substring(0, lastIndex);
+  obj.innerHTML = str;
+}
