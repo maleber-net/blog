80d29abd7596262034731edeedc16012c1d44d45
maleber-net <gun_ip@yahoo.com>
2014-04-28 13:51:10 +0700

    Update maleber.js

--- javascripts/maleber.js
+++ javascripts/maleber.js
@@ -12,7 +12,7 @@ function clearDescById(id) {
   var obj = document.getElementById(id);
   var str = obj.innerHTML;
   str = str.replace(/<(?:.|\n)*?>/gm, '');
-  str = str.substring(0, 350);
+  str = str.substring(0, 400);
   var lastIndex = str.lastIndexOf(" ")
   str = str.substring(0, lastIndex);
   obj.innerHTML = str;
